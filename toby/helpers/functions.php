394f04113da0640fa27a05fe2e45cef849a3a734
Eliott Lebre <eliottlebre@gmail.com>
2026-02-18 12:23:52 +0100

    add: php dotenv support + dd helper

---
+++ toby/helpers/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+use JetBrains\PhpStorm\NoReturn;
+
+if (!function_exists('dd')) {
+    #[NoReturn]
+    function dd(...$var): void
+    {
+        var_dump($var);
+        die();
+    }
+}
+
+
+if (!function_exists('env')) {
+    function env(string $key, $default = null): mixed
+    {
+        return $_ENV[$key] ?? $default;
+    }
+}
